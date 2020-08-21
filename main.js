f65760e76331f2471b60f0bdf89f994fc51aa15f
ntduong10698 <44547589+ntduong10698@users.noreply.github.com>
2020-08-21 20:48:13 +0700

    push

---
+++ main.js
@@ -0,0 +1,30 @@
+let listProduct = [
+    {
+        id: 1,
+        name: "Iphone 10",
+        price: 10000,
+        createDate: "2020-08-20",
+        image: "https://cdn.cellphones.com.vn/media/catalog/product/cache/7/thumbnail/300x/9df78eab33525d08d6e5fb8d27136e95/i/p/iphone11-purple-select-2019.png",
+        introduction: "Iphone tai thỏ",
+        specification: "Chip A11",
+        soldOut: true,
+        guarantee: 12,
+        categoryId: 1,
+        bouth: 1000,
+        promotion: 10
+    },
+    {
+        id: 2,
+        name: "Iphone 11",
+        price: 10000,
+        createDate: "2020-08-20",
+        image: "https://cdn.tgdd.vn/Products/Images/42/200533/iphone-11-pro-max-green-600x600.jpg",
+        introduction: "Iphone tai thỏ",
+        specification: "Chip A11",
+        soldOut: false,
+        guarantee: 12,
+        categoryId: 2,
+        bouth: 1000,
+        promotion: 10
+    },
+];
